746d5ec53089d3b4a134013ef615e42eb335f863
Lukas <lukasabbehb@gmail.com>
2021-03-19 16:20:36 +0100

    Bugs and not fixes

---
+++ TheComputing/Assets/Scripts/Refiner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Refiner : MonoBehaviour
+{
+    //The chosen id in UI of game
+    public GameObject ch_item_prefab;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Detectet");
+        if(collision.gameObject.tag == "Item")
+        {
+            //Gets the component
+            Item item = collision.GetComponent<Item>();
+            item = ch_item_prefab.GetComponent<Item>();
+            gameObject.GetComponent<SpriteRenderer>().sprite = ch_item_prefab.GetComponent<SpriteRenderer>().sprite;
+
+        }
+    }
+}
