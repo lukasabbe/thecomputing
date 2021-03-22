c0a96543775753145c21329c743847499554e791
Edward <edward.bjorkdahl@gmail.com>
2021-03-22 16:23:56 +0100

    shift + scroll för att rotera build direction

---
+++ TheComputing/Assets/Scripts/RotateBuilding.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBuilding : MonoBehaviour
+{
+    public BuildScript buildScript;
+    public int direction = 0;
+
+    void Start()
+    {
+        // Finds the Main Camera and it's BuildScript
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        buildScript = mainCamera.GetComponent<BuildScript>();
+
+        // Sets the buildings direction to the buildDirection
+        direction = buildScript.buildDirection;
+        SetRotation();
+    }
+    void SetRotation()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, -90 * direction);
+    }
+}
