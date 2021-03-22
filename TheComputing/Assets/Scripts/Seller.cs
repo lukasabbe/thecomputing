09f83306e39dba4d6075c5940d14d40c2058be1c
bohlenius <78263552+bohlenius@users.noreply.github.com>
2021-03-22 11:56:21 +0100

    La till så att man kan sälja

---
+++ TheComputing/Assets/Scripts/Seller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seller : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            Debug.Log("Sold " + collision.GetComponent<Item>().ItemName + " for " + collision.GetComponent<Item>().SellPrice + " dollars");
+            Destroy(collision.gameObject);
+        }
+    }
+}
