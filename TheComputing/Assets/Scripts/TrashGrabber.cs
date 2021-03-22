caf8b1b3c976036cabb4b7be71ec3401e4a4857c
Edward <edward.bjorkdahl@gmail.com>
2021-03-22 14:34:20 +0100

    Trash plockare maskin

---
+++ TheComputing/Assets/Scripts/TrashGrabber.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashGrabber : MonoBehaviour
+{
+    public GameObject dropLocation, trashItem;
+    public float dropCooldown = 0; // Cooldown in seconds.
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Trash Area") // The trash grabber will only drop trash when it sits on a trash area
+        {
+            if (dropCooldown < 1) dropCooldown = dropCooldown + 1 * Time.deltaTime;
+            if (dropCooldown >= 1)
+            {
+                GameObject trash;
+                trash = Instantiate(trashItem);
+                trash.transform.position = dropLocation.transform.position;
+                dropCooldown = 0;
+            }
+        }
+    }
+}
