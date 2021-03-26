ef7f72635f13df3bfafef79dae851182d4d21e49
Edward <edward.bjorkdahl@gmail.com>
2021-03-26 15:40:19 +0100

    Auto crafter

--- TheComputing/Assets/Scripts/AutoCrafter.cs
+++ TheComputing/Assets/Scripts/AutoCrafter.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class AutoCrafter : MonoBehaviour
 {
     public GameObject outputItem; // Output item is the item you want to craft, input items are the items required to craft
-    int[] inputID;
     public GameObject dropLocation;
+    List<int> recipeID = new List<int>();
     void Start()
     {
         Item outputItemScript = outputItem.GetComponent<Item>();
 
         if (outputItemScript.id == 9) // Recipe for Circuit Board
         { // Recipe
-            inputID[0] = 8; // trash plastic
-            inputID[1] = 0; // scrap metal
+            recipeID.Add(8); // trash plastic
+            recipeID.Add(0); // scrap metal
         }
     }
     void Update()
@@ -23,50 +23,31 @@ public class AutoCrafter : MonoBehaviour
     }
     void Crafter()
     {
-        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(transform.position, new Vector2(0.498f, 0.498f), 0); // Creates a list of colliders in the tile the cursor is over
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(transform.position, new Vector2(0.498f, 0.498f), 0); // Creates a list of colliders in the tile the cursor is over.
         
-        GameObject[] ITEM = null;
+        //List<GameObject> ITEM = new List<GameObject>();
+        GameObject ITEM1 = null, ITEM2 = null;
+
         foreach (Collider2D hitCollider in hitColliders)
         {
-            if (hitCollider.gameObject.tag == "Item") // Checks if the required items are in the collider
+            if (hitCollider.gameObject.tag == "Item") // Checks if the required items are in the collider.
             {
-                if (hitCollider.gameObject.GetComponent<Item>().id == inputID[0]) ITEM[0] = (hitCollider).gameObject;
-                if (hitCollider.gameObject.GetComponent<Item>().id == inputID[1]) ITEM[1] = (hitCollider).gameObject;
+                
+                int itemID = hitCollider.gameObject.GetComponent<Item>().id;
+                print(itemID);
 
-                // If all required materials are found, craft item
-                if (ITEM[0] != null && ITEM[1] != null)
-                {   // Drop wanted item and delete required items
+                if (itemID == recipeID[0]) ITEM1 = (hitCollider).gameObject;
+                if (itemID == recipeID[1]) ITEM2 = (hitCollider).gameObject;
+
+                // If all required materials are found, craft item.
+                if (ITEM1 != null && ITEM2 != null)
+                {   // Drop wanted item and delete required items.
                     GameObject craftedItem = Instantiate(outputItem);
                     craftedItem.transform.position = dropLocation.transform.position;
-
-                    Destroy(ITEM[0]);
-                    Destroy(ITEM[1]);
+                    Destroy(ITEM1);
+                    Destroy(ITEM2);
                 }
             }
         }
     }
-    /*
-    public void OnTriggerStay2D(Collider2D[] col)
-    {
-        GameObject[] ITEM = null;
-        foreach (Collider2D hitCollider in col)
-        {
-            if (hitCollider.gameObject.tag == "Item") // Checks if the required items are in the collider
-            {
-                if (hitCollider.GetComponent<Item>().id == inputID[0]) ITEM[0] = (hitCollider).gameObject;
-                if (hitCollider.GetComponent<Item>().id == inputID[1]) ITEM[1] = (hitCollider).gameObject;
-            }
-
-            // If all required materials are found, craft item
-            if (ITEM[0] != null && ITEM[1] != null)
-            {   // Drop wanted item and delete required items
-                GameObject craftedItem = Instantiate(outputItem);
-                craftedItem.transform.position = dropLocation.transform.position;
-
-                Destroy(ITEM[0]);
-                Destroy(ITEM[1]);
-            }
-        }
-    }
-    */
 }
