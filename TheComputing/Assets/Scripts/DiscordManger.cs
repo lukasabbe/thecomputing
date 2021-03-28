4133ad999aa0d220fcccb9c84b50a875c632c7a8
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-03-28 18:15:09 +0200

    Fixes with the discord precence

--- TheComputing/Assets/Scripts/DiscordManger.cs
+++ TheComputing/Assets/Scripts/DiscordManger.cs
@@ -7,34 +7,51 @@ public class DiscordManger : MonoBehaviour
     public Discord.Discord discord;
     public Activity activity;
     ActivityManager am;
-
+    private void Awake()
+    {
+        StartCoroutine(waitUp());
+    }
+    private void OnApplicationQuit()
+    {;
+        am.ClearActivity(result => {
+            if (result == Discord.Result.Ok)
+            {
+                Debug.Log("Cleard");
+            }
+            else
+            {
+                Debug.Log("faild");
+            }
+        });
+        discord.Dispose();
+    }
     private void Start()
     {
         discord = new Discord.Discord(825723739874000906, (System.UInt16)Discord.CreateFlags.Default);
+        discord.RunCallbacks();
         am = discord.GetActivityManager();
-        activity = new Discord.Activity
-        {
-            State = "Stats: ",
-            Details = ""
-        };
-        am.UpdateActivity(activity, (res) =>
-        {
-            if(res == Discord.Result.Ok)
-            {
-                Debug.Log("Evryting is very fine");
-            }
-        });
     }
     private void Update()
     {
         discord.RunCallbacks();
-        activity.Details = "money: " + Gamemanager.money.ToString();
+        activity = new Discord.Activity
+        {
+            State = "Is collecting",
+            Details = "Money: " + Gamemanager.money.ToString() + "\nIncome " + Gamemanager.income.ToString()
+        };
+
+    }
+    IEnumerator waitUp()
+    {
+        yield return new WaitForSeconds(10);
         am.UpdateActivity(activity, (res) =>
         {
             if (res == Discord.Result.Ok)
             {
-                Debug.Log("Evryting is very fine");
+                Debug.Log("uppdate ac");
             }
         });
+        StartCoroutine(waitUp());
     }
 }
+
