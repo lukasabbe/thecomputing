14f3baf2750507c13012605651c7f2e583bca34f
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-03-28 17:23:43 +0200

    discord tests

---
+++ TheComputing/Assets/Scripts/DiscordManger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+public class DiscordManger : MonoBehaviour
+{
+    public Discord.Discord discord;
+    public Activity activity;
+    ActivityManager am;
+
+    private void Start()
+    {
+        discord = new Discord.Discord(825723739874000906, (System.UInt16)Discord.CreateFlags.Default);
+        am = discord.GetActivityManager();
+        activity = new Discord.Activity
+        {
+            State = "Stats: ",
+            Details = ""
+        };
+        am.UpdateActivity(activity, (res) =>
+        {
+            if(res == Discord.Result.Ok)
+            {
+                Debug.Log("Evryting is very fine");
+            }
+        });
+    }
+    private void Update()
+    {
+        discord.RunCallbacks();
+        activity.Details = "money: " + Gamemanager.money.ToString();
+        am.UpdateActivity(activity, (res) =>
+        {
+            if (res == Discord.Result.Ok)
+            {
+                Debug.Log("Evryting is very fine");
+            }
+        });
+    }
+}
