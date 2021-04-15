628296226e19d8205aa90cf99324fa528216e57d
Lukas <lukasabbehb@gmail.com>
2021-04-15 17:08:36 +0200

    fix för refiners

--- TheComputing/Assets/Scripts/Refiner.cs
+++ TheComputing/Assets/Scripts/Refiner.cs
@@ -6,32 +6,56 @@ using UnityEngine.UI;
 public class Refiner : MonoBehaviour
 {
     /*
-
-    ----------Item ids----------
+     ----------Item ids----------
 
     ScrapMetal      : 0
     TrashPlastic    : 1
+
     Plastic         : 2
     Polymer         : 3
     TrashCopper     : 4
     TrashGold       : 5
     TrashIron       : 6
     TrashSilver     : 7
+
     Copper          : 8
     Gold            : 9
     Iron            : 10
     Rubber          : 11
     Silver          : 12
-    Cables          : 13
-    Case            : 14
-    CircuitBoard    : 15
-    CPU             : 16
-    GPU             : 17
-    Harddrive       : 18
-    Motherboard     : 19
-    PSU             : 20
-    RAM             : 21
-    SSD             : 22
+
+    Low quality Cables           : 13
+    Low quality Case             : 14
+    Low quality CircuitBoard     : 15
+    Low quality CPU              : 16
+    Low quality GPU              : 17
+    Low quality Harddrive        : 18
+    Low quality Motherboard      : 19
+    Low quality PSU              : 20
+    Low quality RAM              : 21
+    Low quality SSD              : 22
+
+    Cables                       : 23
+    Case                         : 24
+    CircuitBoard                 : 25
+    CPU                          : 26
+    GPU                          : 27
+    Harddrive                    : 28
+    Motherboard                  : 29
+    PSU                          : 30
+    RAM                          : 31
+    SSD                          : 32
+
+    High quality Cables          : 33
+    High quality Case            : 34
+    High quality CircuitBoard    : 35
+    High quality CPU             : 36
+    High quality GPU             : 37
+    High quality Harddrive       : 38
+    High quality Motherboard     : 39
+    High quality PSU             : 40
+    High quality RAM             : 41
+    High quality SSD             : 42
 
     ----------Item ids---------- 
 
@@ -104,6 +128,27 @@ public class Refiner : MonoBehaviour
         {
             return 6;
         }
+        if (pos == 6)//Gold
+        {
+            return 9;
+        }
+        if (pos == 7)//Silver
+        {
+            return 12;
+        }
+        if (pos == 8)//Coppar
+        {
+            return 8;
+        }
+        if (pos == 9)//Iron
+        {
+            return 10;
+        }
+        if (pos == 10)//Polemyr
+        {
+            return 3;
+        }
+
         return 100;
     }
     IEnumerator timer(int waitTime, Collider2D collision, Item i)
@@ -114,8 +159,10 @@ public class Refiner : MonoBehaviour
         collision.GetComponent<Item>().id = i.id;
         collision.GetComponent<Item>().ItemName = i.ItemName;
         collision.GetComponent<Item>().SellPrice = i.SellPrice;
+        collision.GetComponent<Item>().refinsTo = i.refinsTo;
         collision.gameObject.GetComponent<SpriteRenderer>().sprite = ch_item_prefab[ch_item].GetComponent<SpriteRenderer>().sprite;
         collision.gameObject.GetComponent<SpriteRenderer>().color = ch_item_prefab[ch_item].GetComponent<SpriteRenderer>().color;
+
     }
 }
 class obj
